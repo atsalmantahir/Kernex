30c0ed650e7205ee98ba55b56b59d649b8494bd9
atsalmantahir <atstahir@outlook.com>
2026-07-26 13:35:49 +0500

    -- added a Todos controller

---
+++ src/Kernex.API/Controllers/TodosController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kernex.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodosController : ControllerBase
+{
+}
